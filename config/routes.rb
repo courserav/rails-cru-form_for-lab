a5ca181279e438b908fa2be317fae8fcfbd5b925
courserav <lvsmlit@gmail.com>
2021-04-15 17:56:23 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :artists, only: [:index, :show, :new, :edit]
+  post '/artists/', to: 'artists#create'
+  patch '/artists/:id', to: 'artists#update'
+
+  resources :songs, only: [:index, :show, :new, :create, :edit]
+  patch '/songs/:id', to: 'songs#update'
+
+  resources :genres, only: [:index, :show, :new, :create, :edit]
+  patch '/genres/:id', to: 'genres#update'
 end
